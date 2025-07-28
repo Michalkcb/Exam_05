77868dad6f834c0ea15e9bdf0208b986519ba26b
mbany <mbany@student.42.pl>
2025-07-28 18:01:24 +0200

    training hpp

--- new_exam/lvl0/bigint.hpp
+++ new_exam/lvl0/bigint.hpp
@@ -3,38 +3,6 @@
 #include <string>
 #include <iostream>
 #include <sstream>
-#include <math.h>
 #include <algorithm>
 #include <stdexcept>
-
-class Bigint {
-	private:
-		std::string _string;
-	public:
-		Bigint();
-		~Bigint();
-		Bigint(const Bigint& orginal);
-		Bigint(unsigned int n);
-		Bigint(const std::string std) : _string(std) {}
-
-		std::string getValue() const;
-
-		bool operator==(const Bigint& other) const;
-		bool operator!=(const Bigint& other) const;
-		bool operator<=(const Bigint& other) const;
-		bool operator>=(const Bigint& other) const;
-		bool operator<(const Bigint& other) const;
-		bool operator>(const Bigint& other) const;
-
-		Bigint operator+(const Bigint& other) const;
-		Bigint operator++(int);
-		Bigint operator<<(unsigned int value) const;
-
-		Bigint& operator+=(const Bigint& other) const;
-		Bigint& operator++();
-		Bigint& operator<<=(unsigned int value);
-		Bigint& operator>>=(unsigned int value);
-
-
-
-};
+#include <math.h>
