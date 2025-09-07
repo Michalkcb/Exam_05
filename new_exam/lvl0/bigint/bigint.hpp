5c1225dc43e13520be8649c73fc65735466f219e
mbany <mbany@student.42.pl>
2025-09-07 15:35:34 +0200

    IMPROVE COMMENTS

--- new_exam/lvl0/bigint/bigint.hpp
+++ new_exam/lvl0/bigint/bigint.hpp
@@ -14,30 +14,28 @@ private:
     std::string _string;
 
 public:
-    Bigint(); // Domyślny konstruktor
-    Bigint(unsigned int n); // Konstruktor z unsigned int
-    Bigint(const Bigint& original); // Konstruktor kopiujący
-    ~Bigint(); // Destruktor
+    Bigint();                                        // Domyślny konstruktor
+    Bigint(unsigned int n);                          // Konstruktor z unsigned int
+    Bigint(const Bigint& original);                  // Konstruktor kopiujący
+    ~Bigint();                                       // Destruktor
     Bigint(const std::string& str) : _string(str) {} // Konstruktor z stringa
 
-    std::string getValue() const; // Metoda zwracająca wartość jako string
+    std::string getValue() const;                   // Metoda zwracająca wartość jako string
 
     // Operatory porównania
-    Bigint operator+(const Bigint& other) const; // Operator dodawania
-    Bigint operator++(int); // Operator inkrementacji (postfix)
-    Bigint operator<<(unsigned int value) const; // Operator przesunięcia w lewo
-    Bigint operator<<(const Bigint& value) const; // Operator przesunięcia w **lewo**
-    Bigint operator>>(unsigned int value) const;
-    Bigint operator>>(const Bigint& value) const;
+    Bigint operator+(const Bigint& other) const;    // Operator dodawania
+    Bigint operator++(int);                         // Operator inkrementacji (postfix)
+    Bigint operator<<(unsigned int value) const;    // Operator przesunięcia w lewo
+    Bigint operator<<(const Bigint& value) const;   // Operator przesunięcia w **lewo**
+    Bigint operator>>(unsigned int value) const;    // Operator przesunięcia w **prawo**
+    Bigint operator>>(const Bigint& value) const;   // Operator przesunięcia w **prawo**
    
-    Bigint& operator+=(const Bigint& other); // Operator dodawania z przypisaniem
-    Bigint& operator++(); // Operator inkrementacji (prefix)
-    Bigint& operator<<=(unsigned int value); // Operator przesunięcia w lewo z przypisaniem
-    Bigint& operator>>=(unsigned int value); // Operator przesunięcia w prawo z przypisaniem
-    Bigint& operator>>=(const Bigint& value); // Operator przesunięcia w **prawo**
-    Bigint& operator<<=(const Bigint& value); // Operator przesunięcia w lewo z przypisaniem
-
-
+    Bigint& operator+=(const Bigint& other);        // Operator dodawania z przypisaniem
+    Bigint& operator++();                           // Operator inkrementacji (prefix)
+    Bigint& operator<<=(unsigned int value);        // Operator przesunięcia w lewo z przypisaniem
+    Bigint& operator>>=(unsigned int value);        // Operator przesunięcia w prawo z przypisaniem
+    Bigint& operator>>=(const Bigint& value);       // Operator przesunięcia w **prawo**
+    Bigint& operator<<=(const Bigint& value);       // Operator przesunięcia w lewo z przypisaniem
 
     bool operator==(const Bigint& other) const;
     bool operator!=(const Bigint& other) const;
