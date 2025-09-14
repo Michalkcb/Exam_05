9841b98805a208ead75986e8f743adf98be05727
mbany <mbany@student.42.pl>
2025-09-14 13:27:43 +0200

    training day 5

--- new_exam/training/lvl0/bigint/bigint.cpp
+++ new_exam/training/lvl0/bigint/bigint.cpp
@@ -1,1 +1,30 @@
+#include "bigint.hpp"
 
+Bigint::Bigint() : _string("0") {};
+Bigint::Bigint(unsigned int n) {
+
+};
+Bigint::Bigint(const Bigint& orginal) : _string(orginal._string) {};
+Bigint::~Bigint() {};
+
+std::string Bigint::getValue() {return _string;};
+std::ostream& operator<<(std::ostream os, const Bigint& bigint) {
+
+};
+
+bool Bigint::operator==(const Bigint& other) const;
+bool Bigint::operator!=(const Bigint& other) const;
+bool Bigint::operator<(const Bigint& other) const;
+bool Bigint::operator>(const Bigint& other) const;
+bool Bigint::operator<=(const Bigint& other) const;
+bool Bigint::operator>=(const Bigint& other) const;
+
+Bigint Bigint::operator<<(unsigned int value) const;
+Bigint Bigint::operator>>(unsigned int value) const;
+Bigint Bigint::operator>>=(unsigned int value);
+Bigint Bigint::operator<<=(unsigned int value);
+
+Bigint& Bigint::operator<<(const Bigint& other) const;
+Bigint& Bigint::operator>>(const Bigint& other) const;
+Bigint& Bigint::operator<<=(const Bigint& other);
+Bigint& Bigint::operator>>=(const Bigint& other);
