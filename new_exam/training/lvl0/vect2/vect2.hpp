2ade568cb18474729a4f8d6ac11fea98f2002493
mbany <mbany@student.42.pl>
2025-09-21 12:12:34 +0200

    vect2 training day 1

--- new_exam/training/lvl0/vect2/vect2.hpp
+++ new_exam/training/lvl0/vect2/vect2.hpp
@@ -6,7 +6,21 @@
 /*   By: mbany <mbany@student.42warsaw.pl>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/20 17:14:17 by mbany             #+#    #+#             */
-/*   Updated: 2025/09/20 17:14:18 by mbany            ###   ########.fr       */
+/*   Updated: 2025/09/21 12:12:01 by mbany            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#pragma once
+#include <iostream>
+
+class vect2 {
+	private:
+		int x;
+		int y;
+	public:
+		vect2();
+		vect2(int num1, int num2);
+		vect2(const vect2& source);
+		vect2& operator=(const vect2& source);
+		~vect2();
+};
