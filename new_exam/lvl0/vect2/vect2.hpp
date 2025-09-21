aff6f18cc719f5108a96d1964b99f57e78c696be
mbany <mbany@student.42.pl>
2025-09-21 14:56:18 +0200

    vect2 training day 1

--- new_exam/lvl0/vect2/vect2.hpp
+++ new_exam/lvl0/vect2/vect2.hpp
@@ -6,7 +6,7 @@
 /*   By: mbany <mbany@student.42warsaw.pl>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/28 18:20:54 by fatkeski          #+#    #+#             */
-/*   Updated: 2025/09/21 14:21:01 by mbany            ###   ########.fr       */
+/*   Updated: 2025/09/21 14:50:06 by mbany            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,18 +31,18 @@ class vect2
 		
 		vect2& operator=(const vect2& source);
 		
-				vect2& operator+=(const vect2& obj);
-				vect2& operator-=(const vect2& obj);
-				vect2& operator*=(const vect2& obj);
+		vect2& operator+=(const vect2& obj);
+		vect2& operator-=(const vect2& obj);
+		vect2& operator*=(const vect2& obj);
 		
-				vect2 operator+(const vect2& obj) const;
-				vect2 operator-(const vect2& obj) const;
-				vect2 operator*(const vect2& obj) const;
+		vect2 operator+(const vect2& obj) const;
+		vect2 operator-(const vect2& obj) const;
+		vect2 operator*(const vect2& obj) const;
 		
-				vect2 operator-() const;
-				vect2 operator*(int num) const;
+		vect2 operator-() const;
+		vect2 operator*(int num) const;
 		
-				vect2& operator*=(int num);
+		vect2& operator*=(int num);
 				
 		int operator[](int index) const;
 		int& operator[](int index); // NON-COST
--- new_exam/training/lvl0/vect2/vect2.hpp
+++ new_exam/training/lvl0/vect2/vect2.hpp
@@ -6,7 +6,7 @@
 /*   By: mbany <mbany@student.42warsaw.pl>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/20 17:14:17 by mbany             #+#    #+#             */
-/*   Updated: 2025/09/21 14:26:22 by mbany            ###   ########.fr       */
+/*   Updated: 2025/09/21 14:55:44 by mbany            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,44 +14,27 @@
 #include <iostream>
 
 class vect2 {
-	private:
+	public:
 		int x;
 		int y;
-	public:
-		vect2();
-		vect2(int num1, int num2);
-		vect2(const vect2& source);
-		~vect2();
-		
-		bool operator==(const vect2& obj) const;
-		bool operator!=(const vect2& obj) const;
-		
-		vect2& operator=(const vect2& source);
-		
-		//pozostałe operatory---------
-		vect2& operator+=(const vect2& obj);
-		vect2& operator-=(const vect2& obj);
-		vect2& operator*=(const vect2& obj);
-		
-		vect2 operator+(const vect2& obj) const;
-		vect2 operator-(const vect2& obj) const;
-		vect2 operator*(const vect2& obj) const;
-		
-		vect2 operator-() const;
-		vect2 operator*(int num) const;
-		
-		vect2& operator*=(int num);
-		
-		vect2& operator++();
-		vect2& operator--();
-		vect2 operator++(int ) const;
-		vect2 operator--(int ) const;
+	private:
+	vect2();
+	vect2(int num1, int num2);
+	vect2(const vect2& source);
+	~vect2();
 
-		int operator[](int index) const;
-		int& operator[](int index);
-		//---------
-	};
+	vect2& operator=(const vect2& source);
 	
-vect2& operator*(int num, const vect2& obj);
+	vect2& operator+=(const vect2& obj);
+	vect2& operator-=(const vect2& obj);
+	vect2& operator*=(const vect2& obj);
+	
+	vect2 operator+(const vect2& obj) const;
+	vect2 operator-(const vect2& obj) const;
+	vect2 operator*(const vect2& obj) const;
+	
 
-std::ostream& operator<<(std::ostream& os,const vect2& obj);		
+};
+vect2 operator*(int num, const vect2& obj);
+
+std::ostream operator<<(std::ostream os, const vect2& obj);
