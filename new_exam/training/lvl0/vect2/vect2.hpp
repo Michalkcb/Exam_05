2cc317f701acd190cc546ee9bc34cd6577f95d21
mbany <mbany@student.42.pl>
2025-09-23 19:01:27 +0200

    vect2 training day 2

--- new_exam/training/lvl0/vect2/vect2.hpp
+++ new_exam/training/lvl0/vect2/vect2.hpp
@@ -6,7 +6,7 @@
 /*   By: mbany <mbany@student.42warsaw.pl>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/20 17:14:17 by mbany             #+#    #+#             */
-/*   Updated: 2025/09/23 18:26:41 by mbany            ###   ########.fr       */
+/*   Updated: 2025/09/23 19:01:16 by mbany            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,11 +24,24 @@ class vect2 {
 		~vect2();
 
 		vect2& operator=(const vect2& source);
-		
-		bool operator==(const vect2& source) const;
-		bool operator!=(const vect2& source) const;
-		
 
-	
+		bool operator==(const vect2& obj) const;
+		bool operator!=(const vect2& obj) const;
+		
+		vect2& operator+=(const vect2& obj);
+		vect2& operator-=(const vect2& obj);
+		vect2& operator*=(const vect2& obj);
+		
+		vect2 operator+(const vect2& obj) const;
+		vect2 operator-(const vect2& obj) const;
+		vect2 operator*(const vect2& obj) const;
+		
+		int operator[](int index) const;
+		int& operator[](int index);
+		
+		vect2 operator++(int);
+		vect2& operator++();
+		vect2 operator--(int);
+		vect2& operator--();
+		
 };
-std::ostream& operator<<(std::ostream& os, const vect2& obj);
