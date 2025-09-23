f9a1b532423ea3d1a3bd2be3674d0634edb2a4d2
mbany <mbany@student.42.pl>
2025-09-23 17:21:28 +0200

    bigint training day 12

--- new_exam/training/lvl0/bigint/bigint.hpp
+++ new_exam/training/lvl0/bigint/bigint.hpp
@@ -16,3 +16,45 @@ operator+
 operator+=
 operator++
 */
+
+#pragma once
+#include <string>
+#include <iostream>
+#include <sstream>
+#include <algorithm>
+
+class Bigint {
+	private:
+		std::string _string;
+	public:
+		Bigint();
+		Bigint(unsigned int n);
+		Bigint(const Bigint& orginal);
+		~Bigint();
+		Bigint(std::string std) : _string(std) {};
+
+		std::string getValue() const;
+		friend std::ostream& operator<<(const std::ostream& os, const Bigint& bigint);
+
+		bool operator==(const Bigint& other) const;
+		bool operator!=(const Bigint& other) const;
+		bool operator>(const Bigint& other) const;
+		bool operator<(const Bigint& other) const;
+		bool operator>=(const Bigint& other) const;
+		bool operator<=(const Bigint& other) const;
+
+		Bigint operator<<(const Bigint& other) const;
+		Bigint operator>>(const Bigint& other) const;
+		Bigint& operator<<=(const Bigint& other);
+		Bigint& operator>>(const Bigint& other);
+
+		Bigint operator<<(unsigned int value) const;
+		Bigint operator>>(unsigned int value) const;
+		Bigint& operator<<=(unsigned int value);
+		Bigint& operator>>=(unsigned int value);
+
+		Bigint operator+(const Bigint& other) const;
+		Bigint& operator+=(const Bigint& other);
+		Bigint operator++(int);
+		Bigint& operator++();
+};
