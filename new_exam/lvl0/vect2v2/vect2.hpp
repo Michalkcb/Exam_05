5158774ed39114eb434446e19516ca0faf00b0d9
mbany <mbany@student.42.pl>
2025-09-25 19:36:07 +0200

    vect2 training day 3

--- new_exam/lvl0/vect2v2/vect2.hpp
+++ new_exam/lvl0/vect2v2/vect2.hpp
@@ -8,13 +8,17 @@ class vect2{
     public :
         vect2() : x(0), y(0){};
         vect2(int x, int y) : x(x), y(y){};
-        vect2(const vect2& v)
-        {
+        vect2(const vect2& v){
             x = v.x;
             y = v.y;
         }
-        vect2& operator=(const vect2 &v)
-        {
+        bool operator==(const vect2& v){
+            return (x == v.x && y == v.y);
+        }
+        bool operator!=(const vect2& v){
+            return !(*this == v);
+        }
+        vect2& operator=(const vect2 &v){
             if (*this != v)
             {
                 x = v.x;
@@ -22,106 +26,71 @@ class vect2{
             }
             return (*this);
         } 
+        int& operator[](int i){
+            return (i == 0 ? x : y);
+        }
+        int operator[](int i) const{
+            return (i == 0 ? x : y);
+        }
         ~vect2(){};
-        
-        friend std::ostream& operator<<(std::ostream &COUT, const vect2& v)
-        {
+        friend std::ostream& operator<<(std::ostream &COUT, const vect2& v){
             COUT << "{" << v[0] << ", " << v[1] << "}";
             return COUT;
         }
-
-        int& operator[](int i)
-        {
-            return (i == 0 ? x : y);
-        }
-
-        int operator[](int i) const
-        {
-            return (i == 0 ? x : y);
-        }
-
-        bool operator==(const vect2& v)
-        {
-            return (x == v.x && y == v.y);
-        }
-
-        bool operator!=(const vect2& v)
-        {
-            return !(*this == v);
-        }
-
-        vect2 operator++(int)
-        {
+        vect2 operator++(int){
             vect2 tmp = *this;
             x += 1;
             y += 1;
             return tmp;
         }
-
-        vect2& operator++()
-        {
+        vect2& operator++(){
             x += 1;
             y += 1;
             return *this;
         }
-
-        vect2 operator--(int)
-        {
+        vect2 operator--(int){
             vect2 tmp = *this;
             x -= 1;
             y -= 1;
             return tmp;
         }
-    
-        vect2& operator--()
-        {
+        vect2& operator--(){
             x -= 1;
             y -= 1;
             return *this;
         }
-
-        vect2& operator+=(const vect2& v)
-        {
+        vect2& operator+=(const vect2& v){
             x += v.x;
             y += v.y;
             return *this;
         }
-
-        vect2& operator-=(const vect2& v)
-        {
+        vect2& operator-=(const vect2& v){
             x -= v.x;
             y -= v.y;
             return *this;
         }
-
-        vect2& operator*=(int n)
-        {
+        vect2& operator*=(int n){
             x *= n;
             y *= n;
             return *this;
         }
-         vect2 operator+(const vect2& v) const
-         {
+         vect2 operator+(const vect2& v) const{
             return vect2(x + v.x, y +v.y);
          }
 
-         vect2 operator-(const vect2& v) const
-         {
+         vect2 operator-(const vect2& v) const{
             return vect2(x - v.x, y - v.y);
          }
 
-         vect2 operator*(int n) const
-         {
+         vect2 operator*(int n) const{
             return vect2(x * n, y * n);
          }
 
-         friend vect2 operator*(int n, const vect2 &v)
-         {
+         friend vect2 operator*(int n, const vect2 &v){
             return vect2(v.x * n, v.y * n);
          }
 
-         vect2 operator-() const
-         {
+         vect2 operator-() const{
             return vect2(-x, -y);
          }
 };
