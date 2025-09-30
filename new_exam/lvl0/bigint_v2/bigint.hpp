c33320ef903b055112a43fb74689eed6eaf91e02
Michalkcb <michalkcb@gmail.com>
2025-09-30 20:10:42 +0200

    train 2

--- new_exam/lvl0/bigint_v2/bigint.hpp
+++ new_exam/lvl0/bigint_v2/bigint.hpp
@@ -6,64 +6,132 @@
 /*   By: mbany <mbany@student.42warsaw.pl>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/23 14:17:26 by fatkeski          #+#    #+#             */
-/*   Updated: 2025/09/07 13:37:28 by mbany            ###   ########.fr       */
+/*   Updated: 2025/09/30 14:35:53 by mbany            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef BIGINT
-#define BIGINT
-
-#include <sstream>
-#include <iostream>
+#pragma once
 #include <string>
-#include <cstdlib>
+#include <iostream>
+#include <ostream>
+#include <sstream>
+#include <algorithm>
 
-class bigint
-{
+class bigint {
 	private:
-		std::string str;
-		//std::string result;
+		std::string _string;
 	public:
-		bigint();
-		bigint(unsigned int num);
-		bigint(const bigint& source);
+		bigint() : _string("0") {};
+		bigint(unsigned int n) {
+			std::ostringstream oss;
+			oss << n;
+			_string = oss.str();
+		};
+		bigint(const bigint& orginal) : _string(orginal._string) {};
+		~bigint() {};
+		bigint(std::string std) : _string(std) {};
 
-		std::string getStr()const;
+		std::string getValue() const {return _string;};
+		friend std::ostream& operator<<(std::ostream& os, const bigint& bigint) {
+			std::string value = bigint.getValue();
+			size_t first_non_zero = value.find_first_not_of('0');
+			if (first_non_zero == std::string::npos)
+				os << "0";
+			else
+				os << value.substr(first_non_zero);
+			return os;
+		};
 
-		bigint& operator=(const bigint& source);
+		bool operator==(const bigint& other) const {
+			return this->_string == other._string;
+		};
+		bool operator!=(const bigint& other) const {
+			return this->_string != other._string;
+		};
+		bool operator<(const bigint& other) const {
+			if (this->_string.length() != other._string.length())
+				return (this->_string.length() < other._string.length());
+			return (this->_string < other._string);
+		};
+		bool operator>(const bigint& other) const {
+			return other < *this;
+		};
+		bool operator>=(const bigint& other) const {
+			return !(*this < other);
+		};
+		bool operator<=(const bigint& other) const {
+			return !(*this > other);
+		};
+		bigint operator<<(unsigned int value) const {
+			bigint result(*this);
+			result._string.append(value, '0');
+			return result;
+		};
+		bigint operator>>(unsigned int value) const {
+			if (value >= this->_string.length())
+				return bigint(0);
+			std::string result = _string.substr(0, _string.length() - value);
+			return bigint(result);
+		};
+		bigint& operator<<=(unsigned int value) {
+			*this = *this << value;
+			return *this;
+		};
+		bigint& operator>>=(unsigned int value) {
+			*this = *this >> value;
+			return *this;
+		};
+		bigint operator<<(const bigint& other) const {
+			unsigned int shift = std::stoi(other.getValue());
+			return *this << shift;
+		};
+		bigint operator>>(const bigint& other) const {
+			unsigned int shift = std::stoi(other.getValue());
+			return *this >> shift;
+		};
+		bigint& operator<<=(const bigint& other) {
+			unsigned int shift = std::stoi(other.getValue());
+			return *this <<= shift;
+		};
+		bigint& operator>>=(const bigint& other) {
+			unsigned int shift = std::stoi(other.getValue());
+			return *this >>= shift;
+		};
 
-		// addition
-		bigint operator+(const bigint& other)const;
-		bigint& operator+=(const bigint& other);
 
-		// increments
-		bigint& operator++(); // ++x
-		bigint operator++(int); // x++
+		bigint operator+(const bigint& other) const {
+			std::string a = this->_string;
+			std::string b= other._string;
 
-		// shift with num
-		bigint operator<<(unsigned int n)const;
-		bigint operator>>(unsigned int n)const;
-		bigint& operator<<=(unsigned int n);
-		bigint& operator>>=(unsigned int n);
+			std::string result = "";
+			int carry = 0;
+			
+			int i = a.length() -1;
+			int j = b.length() -1;
 
-		//shift with object
-		bigint operator<<(const bigint& other)const;
-		bigint operator>>(const bigint& other)const;
-		bigint& operator<<=(const bigint& other);
-		bigint& operator>>=(const bigint& other);
-
-		// ==, !=, <, >, <=, >=
-		bool operator==(const bigint& other) const;
-		bool operator!=(const bigint& other) const;
-		bool operator<(const bigint& other) const;
-		bool operator>(const bigint& other) const;
-		bool operator<=(const bigint& other) const;
-		bool operator>=(const bigint& other) const;
-
-		// ~bigint();
-
+			while (i >= 0 || j >= 0 || carry > 0) {
+				int sum = carry;
+				if (i >= 0)
+					sum += a[i--] -'0';
+				if (j >= 0)
+					sum += b[j--] -'0';
+				result.push_back((sum % 10) + '0');
+				carry = sum / 10;
+			}
+			std::reverse(result.begin(), result.end());
+			return bigint(result);
+		};
+		bigint& operator+=(const bigint& other) {
+			*this = *this + other;
+			return *this;
+		};
+		bigint operator++(int) {
+			bigint tmp = *this;
+			++(*this);
+			return tmp;
+		};
+		bigint& operator++() {
+			*this = *this + bigint(1);
+			return *this;
+		};
 };
-
-std::ostream& operator<<(std::ostream& output, const bigint& obj);
-
-#endif
