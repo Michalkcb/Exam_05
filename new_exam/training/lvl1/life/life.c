8ea78dd688370b0dd37805936374d2773fff69a6
Michalkcb <michalkcb@gmail.com>
2025-10-01 22:17:55 +0200

    life training day 1

--- new_exam/training/lvl1/life/life.c
+++ new_exam/training/lvl1/life/life.c
@@ -1,11 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   life.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mbany <mbany@student.42warsaw.pl>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/10/01 22:17:31 by mbany             #+#    #+#             */
+/*   Updated: 2025/10/01 22:17:38 by mbany            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include <stdio.h>
 #include <unistd.h>
 #include <stdlib.h>
 
 int main (int ac, char **av) {
-    if ac != 4 {
+    if (ac != 4)
         return 1;
-    }
     int width = atoi(av[1]);
     int height = atoi(av[2]);
     int iterations = atoi(av[3]);
@@ -30,32 +41,39 @@ int main (int ac, char **av) {
             x++;
         else if (cmd == 'x') 
             prt = !prt;
-        arr[0][y][x] = 1;
+        if (prt)
+            arr[0][y][x] = 1;
     }
     for (int iter = 0; iter < iterations; iter++) {
         for ( int h = 1; h <= height; h++) {
             for (int w = 1; w <= width; w++) {
                 int n = 0;
-                for (int y = -1, y <= 1, y++) {
+                for (int y = -1; y <= 1; y++) {
                     for (int x = -1; x <= 1; x++) {
                         if (!(x==0 && y == 0)){
                             n += arr[iter % 2][y+h][x+w];
                         }
                     }
-                    if (arr[iter % 2][h][w] == 1) {
-                        if (n==2 || n==3)
-                            arr[(iter + 1) % 2][h][w] == 1;
-                        else
-                            arr[(iter + 1) % 2][h][w] == 0;
-                    }
-                    else {
-                        if (n==3)
-                            arr[(iter + 1) % 2][h][w] == 1;
-                        else
-                            arr[(iter + 1) % 2][h][w] == 0;
-                    }
+                }
+                if (arr[iter % 2][h][w] == 1) {
+                    if (n==2 || n==3)
+                        arr[(iter + 1) % 2][h][w] = 1;
+                    else
+                        arr[(iter + 1) % 2][h][w] = 0;
+                }
+                else {
+                    if (n==3)
+                        arr[(iter + 1) % 2][h][w] = 1;
+                    else
+                        arr[(iter + 1) % 2][h][w] = 0;
                 }
             }
         }
     }
+    for (int i = 1; i <= height; i++) {
+        for (int j = 1; j <= width; j++)
+            putchar(arr[iterations % 2][i][j] ? 'O' : ' ');
+        putchar('\n');
+    }
+    return 0;
 }
