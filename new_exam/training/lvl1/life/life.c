fb69ac109b9fe844dc6190d78e55ba23c2690828
Michalkcb <michalkcb@gmail.com>
2025-10-02 14:20:35 +0200

    life training day 2

--- new_exam/training/lvl1/life/life.c
+++ new_exam/training/lvl1/life/life.c
@@ -4,33 +4,37 @@
 
 int main (int ac, char **av) {
     if (ac != 4)
-        return 1;
+        return 0;
     int width = atoi(av[1]);
     int height = atoi(av[2]);
     int iterations = atoi(av[3]);
-    arr[2][height + 2][width + 2];
-    for (int i = 0; i < 2; i++) {
-        for (int y = 0; y < height +2; y++) {
-            for (int x = 0; x < width +2; x++)
+    int arr[2][height + 2][width + 2];
+
+    for (int i = 0; i < 2; i++)
+        for (int y = 0; y < height + 2; y++)
+            for (int x = 0; x < width + 2; x++)
                 arr[i][y][x] = 0;
-        }
-    }
-    int x=1, y=1;
+    int x = 1, y=1;
     int prt = 0;
     char cmd;
     while (read(0, &cmd, 1) > 0) {
-        if (cmd == "w" && y > 1)
+        if (cmd == 'w' && y >1)
             y--;
-        else if (cmd == "a" && x > 1)
+        else if (cmd == 'a' && x >1)
             x--;
-        else if (cmd == "s" && y < height)
+        else if (cmd == 's' && y < height)
+            y++;
+        else if (cmd == 'd' && x < width)
             x++;
-        else if (cmd == "d" && x < width)
-            x--;
-        else if (cmd == "x")
+        else if (cmd == 'x')
             prt = !prt;
-        if (ptr) 
-            arr[i][y][x] = 1;
+        if (prt)
+            arr[0][y][x] = 1;
     }
-
+    for (int i = 1; i <= height; i++) {
+        for (int j = 1; j < width; j++)
+            putchar(arr[iterations % 2][i][j] ? 'O' : ' ');
+        putchar('\n');
+    }
+    return 0;
 }
