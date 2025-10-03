7bebd5a45c11f610e81caea9bb3d081f56a47799
Michalkcb <michalkcb@gmail.com>
2025-10-03 17:47:56 +0200

    life training day 3

--- new_exam/training/lvl1/life/life.c
+++ new_exam/training/lvl1/life/life.c
@@ -2,57 +2,57 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main (int ac, char **av) {
-    if (ac!= 4)
+int main(int ac, char **av) {
+    if(ac!=4)
         return 1;
     int width = atoi(av[1]);
     int height = atoi(av[2]);
     int iterations = atoi(av[3]);
-    int arr[2][height + 2][width + 2];
-
-    for(int i = 0; i < 2; i ++) {
-        for (int y = 0; y < height +2; y++)
+    int arr[2][height+2][width+2];
+    
+    for(int i = 0; i < 2; i++) {
+        for(int y = 0; y < height + 2; y++)
             for(int x = 0; x < width + 2; x++)
                 arr[i][y][x] = 0;
     }
-    int x=1, y=1;
+
+    int x=1,y=1;
     int prt = 0;
     char cmd;
 
     while(read(0, &cmd, 1) > 0) {
-        if (cmd == 'w' && y > 1)
+        if(cmd=='w' && y > 1)
             y--;
-        else if (cmd == 'a' && x > 1)
+        else if(cmd=='a' && x > 1)
             x--;
-        else if (cmd == 's' && y < height)
+        else if(cmd=='s' && y < height)
             y++;
-        else if (cmd == 'd' && x < width)
+        else if(cmd=='d' && x < width)
             x++;
-        else if (cmd == 'x')
+        else if(cmd=='x')
             prt = !prt;
-        if (prt)
+        if(prt)
             arr[0][y][x] = 1;
     }
-
-    for(int iter = 0; iter < iterations; iter++) {
-        for (int h = 1; h <=height; h++)
+    for (int iter = 0; iter < iterations; iter++) {
+        for(int h = 1; h <= height; h++)
             for(int w = 1; w <= width; w++) {
                 int n = 0;
-                for(int y = -1; y <=1; y++)
-                    for(int x = -1; x <=1; x++)
-                        if(!(x==0 && y == 0))
-                            n += arr[iter%2][h+y][w+x];
-                if(arr[iter%2][h][w] == 1) {
+                for(int y = -1; y <= 1; y++)
+                    for(int x = -1; x <= 1; x++)
+                        if(!(x==0 && y==0))
+                            n+=arr[iter%2][h+y][x+w];
+                if(arr[iter%2][h][w] == 1){
                     if(n==2 || n==3)
-                        arr[(iter+1)%2][h][w] == 1;
+                        arr[(iter+1)%2][h][w] = 1;
                     else
-                        arr[(iter+1)%2][h][w] == 0;
+                        arr[(iter+1)%2][h][w] = 0;
                 }
-                else {
+                else{
                     if(n==3)
-                        arr[(iter+1)%2][h][w] == 1;
+                        arr[(iter+1)%2][h][w] = 1;
                     else
-                        arr[(iter+1)%2][h][w] == 0;
+                        arr[(iter+1)%2][h][w] = 0;
                 }
             }
     }
